f53451aa5bf0cbf099767204941b30dab25bd6fa
CrafterPika <g.mueller.warngau@gmail.com>
2024-10-26 14:27:14 +0200

    oh for fucks sake

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def errorhandler(e):
 if __name__ == "__main__":
     from waitress import serve
 
-    print("syscheck_receiver v1.0.3")
+    print(f"syscheck_receiver v{config['version']}")
 
     # check if sqlite db exist if not make one
     if not os.path.isfile(db_dir):
